5b126404cfcac12ba91869ccf571c1a2daff484c
Aleksey <ach1010110@yandex.ru>
2022-08-27 20:09:26 +0300

    test commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const human = {
+    name: "Aleksey",
+    age: 36,
+    gitUser: true
+}
+
+console.table(human)
+
+human.weight = 90
+
+console.table(human)
+
+delete human.weight
+
+console.log (human)
+
+human["weight"] = 90
+
+console.log (human)
+
+const humanFace = "face"
+const haveWork = "work"
+
+human[humanFace] = "nice"
+human[haveWork] = true
+
+console.table(human)
+
+human.pc = {
+    laptop: true,
+    desktop: true
+}
+
+console.table(human)
+console.log(human)
+
+human.clothes = {
+    pans: 'military',
+    shorts: 'red'
+}
+
+console.log(human)
