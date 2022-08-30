294c03c8c5a63f23826c680ac7d88bb7e3fcb61c
Aleksey <ach1010110@yandex.ru>
2022-08-30 23:29:45 +0300

    solve task #5

--- tasks/task5.js
+++ tasks/task5.js
@@ -4,56 +4,51 @@ const fs = require("fs");
 
 fs.readFile("tasks/text4task5.txt", (err, data) => {
     if (err) throw err;
-    //console.log(data.toString());
 
-    const text = data.toString();
-
-    function numToWords (txt) {
-        let result = '';
-        for (let index = 0; index < txt.length; index++) {
-            if (txt.charAt(index) == '0') {
-                result += 'ноль';
-            } else if (txt.charAt(index) == '1') {
-                result += 'один';
-            } else if (txt.charAt(index) == '2') {
-                result += 'два';
-            } else if (txt.charAt(index) == '3') {
-                result += 'три';
-            } else if (txt.charAt(index) == '4') {
-                result += 'четыре';
-            } else if (txt.charAt(index) == '5') {
-                result += 'пять';
-            } else if (txt.charAt(index) == '6') {
-                result += 'шесть';
-            } else if (txt.charAt(index) == '7') {
-                result += 'семь';
-            } else if (txt.charAt(index) == '8') {
-                result += 'восемь';
-            } else if (txt.charAt(index) == '9') {
-                result += 'девять';
-            } else {
-                result += txt.charAt(index);
-            }
-        }
-
-        return result;
-    }
-
-    function allFirstLettersUp (txt) {
-        let result = '';
-        for (let index = 0; index < text.length; index++) {
-            if ((text.charAt(index-1) == ' ') && (index > 0)) {
-                result += text.charAt(index).toUpperCase();
-            } else {
-                result += text.charAt(index);
-            }
-        }
-
-        return result;
-    }
-    
-    console.log(allFirstLettersUp(numToWords(text)));
+    const onlyWords = numToWords(data.toString());
+    console.log(allFirstLettersUp(onlyWords));
 });
 
+function allFirstLettersUp (txt) {
+    let result = '';
+    for (let index = 0; index < txt.length; index++) {
+        if ((txt.charAt(index-1) == ' ') && (index > 0)) {
+            result += txt.charAt(index).toUpperCase();
+        } else {
+            result += txt.charAt(index);
+        }
+    }
 
+    return result;
+}
 
+function numToWords (txt) {
+    let result = '';
+    for (let index = 0; index < txt.length; index++) {
+        if (txt.charAt(index) == '0') {
+            result += 'ноль';
+        } else if (txt.charAt(index) == '1') {
+            result += 'один';
+        } else if (txt.charAt(index) == '2') {
+            result += 'два';
+        } else if (txt.charAt(index) == '3') {
+            result += 'три';
+        } else if (txt.charAt(index) == '4') {
+            result += 'четыре';
+        } else if (txt.charAt(index) == '5') {
+            result += 'пять';
+        } else if (txt.charAt(index) == '6') {
+            result += 'шесть';
+        } else if (txt.charAt(index) == '7') {
+            result += 'семь';
+        } else if (txt.charAt(index) == '8') {
+            result += 'восемь';
+        } else if (txt.charAt(index) == '9') {
+            result += 'девять';
+        } else {
+            result += txt.charAt(index);
+        }
+    }
+
+    return result;
+}
