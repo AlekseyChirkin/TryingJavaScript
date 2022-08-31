a84cf459f069386f528b9af53866f8e59622812f
Aleksey <ach1010110@yandex.ru>
2022-08-31 23:19:47 +0300

    task 7 file added

--- tests_js_code.js
+++ tests_js_code.js
@@ -5,19 +5,18 @@ const jsStudent = {
     language: 'RU',
 }
 try {
-    setTimeout(()=>{throw new Error('Прошло 3 секунды со старта программы, хватит!!!')}, 3000);
-    throw new Error ('УУУУ!! ска!!! ОШИБКА!');
+    setTimeout(() => {throw new Error('Прошло 3 секунды со старта программы, хватит!!!')}, 3000);
+    
+    throw new Error ('ОШИБКА!');
 } catch (error) {
     // console.error(error);
     console.log('УУУУУ!!! Попался, ска! Вот что орёт: "' + error.message + '"');
 }
 
-
 const jsStudentIsStudyingTime = (student, studyTime = Date()) => ({
     ...student,
     studyTime,
 })
-
 
 const jsStudentIsStudyingTime2 = (student, studyTime = Date()) =>  {return {
     ...student,
